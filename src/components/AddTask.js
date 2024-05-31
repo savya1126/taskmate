149023e3c2351e255e186cb7cc61ae5517406626
savyasapkota13 <savyasapkota13@gmail.com>
2024-05-31 11:27:36 +0545

    Added all the functions of taskLists required

---
+++ src/components/AddTask.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const AddTask = ({ tasklist, setTaskList, task, setTask }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (task.id) {
+      const date = new Date();
+      const updatedTaskList = tasklist.map((todo) =>
+        todo.id === task.id
+          ? {
+              id: task.id,
+              name: task.name,
+              time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+            }
+          : todo
+      );
+      setTaskList(updatedTaskList);
+      setTask({});
+    } else {
+      const date = new Date();
+      const newTask = {
+        id: date.getTime(),
+        name: e.target.task.value,
+        time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+      };
+      setTaskList([...tasklist, newTask]);
+      setTask({});
+    }
+  };
+
+  return (
+    <section className="addTask">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="task"
+          placeholder="Add task"
+          autoComplete="off"
+          value={task.name || ""}
+          onChange={(e) => setTask({ ...task, name: e.target.value })}
+        />
+        <button type="submit">{task.id ? "Update" : "Add"}</button>
+      </form>
+    </section>
+  );
+};
+
+export default AddTask;
